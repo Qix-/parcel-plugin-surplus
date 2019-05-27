a18fa80d0bcd4c0c2c1ca640cb5d1f7c9c0e522e
Qix <Qix-@users.noreply.github.com>
2019-05-27 15:47:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function (bundler) {
   const jsAsset = require.resolve('./lib/js-asset.js')
 
-  bundler.addAssetType('js', jsAsset)
+  //bundler.addAssetType('js', jsAsset)
   bundler.addAssetType('jsx', jsAsset)
-  bundler.addAssetType('es6', jsAsset)
-  bundler.addAssetType('jsm', jsAsset)
-  bundler.addAssetType('mjs', jsAsset)
+  //bundler.addAssetType('es6', jsAsset)
+  //bundler.addAssetType('jsm', jsAsset)
+  //bundler.addAssetType('mjs', jsAsset)
 }
